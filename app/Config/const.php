2434f091eff34f5d7cae2a342e49570274502ead
福冨翔 <hnut.fukutomi@gmail.com>
2019-12-10 10:50:22 +0800

    remove ignore

--- app/Config/const.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('TEISU','100');
-define('TRELLO_KEY','34a2f2656df485531bb44bb6b3699ea6');
-define('TRELLO_TOKEN','2fb661fa1054e60475e9ef8488435c2e89567ef25561fca2a8268601de098575');
-define('TRELLO_BOARD_ID','5b4c46b1487f267986cc3c56');
-define('BACKLOG_API_KEY','H53eJK4oE4OnQLwibkjyihRGgnVxNZsCx4akeQ5uOhJqmNkR3GvKIkoJmONrwy6O');
-define('BACKLOG_URL','https://love.backlog.jp/');
-define('PROJECT_ID','25169');
